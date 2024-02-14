226cec7f2a69955ad8fed2b8d6a7c185558ff150
MrGrouper <60527628+MrGrouper@users.noreply.github.com>
2024-02-14 00:29:50 -0600

    change cookie domain

--- backend/src/controllers/user-controllers.ts
+++ backend/src/controllers/user-controllers.ts
@@ -46,7 +46,7 @@ export const userSignup = async (
     expires.setDate(expires.getDate() + 7);
     res.cookie(COOKIE_NAME, token, {
       path: "/",
-      domain: "onrender.com",
+      domain: "u4e.onrender.com",
       expires,
       httpOnly: true,
       signed: true,
