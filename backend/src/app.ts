bfaabacd37c923c2edae535438280b13208d4025
MrGrouper <60527628+MrGrouper@users.noreply.github.com>
2024-02-13 19:37:17 -0600

    vite config updated

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -14,24 +14,7 @@ const app = express();
 
 //middlewares
 
-// app.use(cors({    
-//     credentials: true
-// }));
-var allowedOrigins = ['http://localhost:5173',
-                      'https://u4e.onrender.com/'];
-app.use(cors({
-  origin: function(origin, callback){
-    // allow requests with no origin 
-    // (like mobile apps or curl requests)
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){
-      var msg = 'The CORS policy for this site does not ' +
-                'allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  }
-}));
+app.use(cors<Request>());
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
 
